ebedd3bb5762ecd82eb190b9df73eca91fb35251
Mixces <70869960+Mixces@users.noreply.github.com>
2022-05-28 11:23:14 -0800

    something (idk)

--- src/main/java/cc/woverflow/overflowanimations/mixin/ForgeHooksClientMixin.java
+++ src/main/java/cc/woverflow/overflowanimations/mixin/ForgeHooksClientMixin.java
@@ -6,24 +6,14 @@ import net.minecraft.client.resources.model.IBakedModel;
 import net.minecraftforge.client.ForgeHooksClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import javax.vecmath.Matrix4f;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = ForgeHooksClient.class, remap = false)
 public class ForgeHooksClientMixin {
 
-    @Redirect(method = "handleCameraTransforms", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/ForgeHooksClient;multiplyCurrentGlMatrix(Ljavax/vecmath/Matrix4f;)V"))
-    private static void onHandlePerspective(Matrix4f i, IBakedModel model, ItemCameraTransforms.TransformType cameraTransformType) {
-        if (!MixcesHandler.applyMixcesTransformations(cameraTransformType)) {
-            ForgeHooksClient.multiplyCurrentGlMatrix(i);
-        }
-    }
-
-    @Redirect(method = "handleCameraTransforms", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/model/ItemCameraTransforms;applyTransform(Lnet/minecraft/client/renderer/block/model/ItemCameraTransforms$TransformType;)V", remap = true))
-    private static void onApplyTransformations(ItemCameraTransforms instance, ItemCameraTransforms.TransformType type) {
-        if (!MixcesHandler.applyMixcesTransformations(type)) {
-            instance.applyTransform(type);
-        }
+    @Inject(method = "handleCameraTransforms", at = @At("RETURN"))
+    private static void onHandlePerspective(IBakedModel model, ItemCameraTransforms.TransformType cameraTransformType, CallbackInfoReturnable<IBakedModel> cir) {
+        MixcesHandler.applyMixcesTransformations(cameraTransformType);
     }
 }
