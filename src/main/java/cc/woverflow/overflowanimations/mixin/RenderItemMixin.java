698fe1f625c2596077237c815b6dfd8269938e78
Mixces <70869960+Mixces@users.noreply.github.com>
2022-05-28 11:21:44 -0800

    first person item positions + bug fix

--- src/main/java/cc/woverflow/overflowanimations/mixin/RenderItemMixin.java
+++ src/main/java/cc/woverflow/overflowanimations/mixin/RenderItemMixin.java
@@ -2,6 +2,9 @@ package cc.woverflow.overflowanimations.mixin;
 
 import cc.woverflow.overflowanimations.MixcesHandler;
 import cc.woverflow.overflowanimations.OverflowAnimations;
+import cc.woverflow.overflowanimations.config.AnimationsConfig;
+import net.minecraft.block.*;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.client.resources.model.IBakedModel;
@@ -18,8 +21,45 @@ public class RenderItemMixin {
 
     @Inject(method = "renderItemModelTransform", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GlStateManager;pushMatrix()V", shift = At.Shift.AFTER))
     private void setRenderingStack(ItemStack stack, IBakedModel model, ItemCameraTransforms.TransformType cameraTransformType, CallbackInfo ci) {
-        OverflowAnimations.renderingStack = stack;
+        if (cameraTransformType == ItemCameraTransforms.TransformType.THIRD_PERSON) {
+            OverflowAnimations.renderingStack = stack;
+        } else if (AnimationsConfig.mixcesAnimations && stack.getItem() != null) {
+            GlStateManager.translate(-0.01, 0.002, 0.0005);
+            GlStateManager.rotate(0, 0, 1, 0);
+            GlStateManager.rotate(0, 1, 0, 0);
+            GlStateManager.rotate(0, 0, 0, 1);
+            GlStateManager.scale(1, 1, 1);
+            if (stack.getItem() instanceof ItemBlock) {
+                Block block = ((ItemBlock) stack.getItem()).getBlock();
+                if (AnimationsConfig.firstPersonCarpetPosition && (block instanceof BlockCarpet || block instanceof BlockSnow)) {
+                    GlStateManager.translate(0.015, -0.331, -0.0005);
+                    GlStateManager.rotate(0, 0, 1, 0);
+                    GlStateManager.rotate(0, 1, 0, 0);
+                    GlStateManager.rotate(0, 0, 0, 1);
+                    GlStateManager.scale(1, 1, 1);
+                } else if (block instanceof BlockLadder || block instanceof BlockPane || block instanceof BlockRail || block instanceof BlockRailPowered || block instanceof BlockRailDetector || block instanceof BlockVine || block instanceof BlockWeb || block instanceof BlockLever || block instanceof BlockBush || block instanceof BlockTorch || block instanceof BlockTripWireHook) {
+                    GlStateManager.translate(0, 0, 0.0005);
+                    GlStateManager.rotate(0, 0, 1, 0);
+                    GlStateManager.rotate(0, 1, 0, 0);
+                    GlStateManager.rotate(0, 0, 0, 1);
+                    GlStateManager.scale(1, 1, 1);
+                } else if (block instanceof BlockFurnace || block instanceof BlockDispenser || block instanceof BlockPumpkin || block instanceof BlockChest || block instanceof BlockEnderChest || block instanceof BlockFence || block instanceof BlockFenceGate) {
+                    GlStateManager.translate(0.015, -0.004, -0.0005);
+                    GlStateManager.rotate(0, 0, 1, 0);
+                    GlStateManager.rotate(0, 1, 0, 0);
+                    GlStateManager.rotate(0, 0, 0, 1);
+                    GlStateManager.scale(1, 1, 1);
+                } else if (block != null) {
+                    GlStateManager.translate(0.015, -0.004, -0.0005);
+                    GlStateManager.rotate(90, 0, 1, 0);
+                    GlStateManager.rotate(0, 1, 0, 0);
+                    GlStateManager.rotate(0, 0, 0, 1);
+                    GlStateManager.scale(1, 1, 1);
+                }
+            }
+        }
     }
+
 
     @Dynamic("OptiFine")
     @Redirect(method = "renderItemModelTransform", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/model/ItemCameraTransforms;applyTransform(Lnet/minecraft/client/renderer/block/model/ItemCameraTransforms$TransformType;)V"))
