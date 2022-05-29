09a0fe9f3945cdab63255804028d0c74942db440
Mixces <70869960+Mixces@users.noreply.github.com>
2022-05-28 16:11:20 -0800

    revamp mixces animations (#1)

---
+++ README.md
@@ -0,0 +1,23 @@
+<div align="center">
+
+# OverflowAnimations
+
+<a href="https://github.com/W-OVERFLOW/OverflowAnimations/releases" target="_blank"></a>
+<img alt="downloads" src="https://img.shields.io/github/downloads/W-OVERFLOW/OverflowAnimations/total?color=F5C400&style=for-the-badge" /> <img alt="downloads latest" src="https://img.shields.io/github/downloads-pre/W-OVERFLOW/OverflowAnimations/latest/total?color=F5C400&style=for-the-badge" />
+
+The perfect companion mod to Sk1er's Old Animations Mod! We've added necessary features to amplify the 1.7 experience perpetuated by Sk1er's amazing mod. This mod requires Sk1er's Old Animations Mod to also be present in your mods folder! Your game WILL crash if Sk1er's OAM isn't found.
+
+# Features
+
+  **Cleaner Enchantment Glint** - Make the enchantment glint only display up to down. 
+
+  **1.15+ Armor Enchantment Glint** - Make the armor enchantment glint like the 1.15 animation.
+
+  **1.7 Third Person Blockhit Rendering** - Render the third person blockhit like in 1.7.
+
+  **Spiderfrog Old Animations Punching** - Enable 1.7 item usage whilst punching like Spiderfrog's Old Animations. Fixes false anti-cheat detections.
+
+  **1.7 Item / Block Positions** - Uses the 1.7 item positions for all items and blocks in both the first and third person POV.
+
+  **1.7 First Person Carpet Position** - Lowers the carpets in the first person POV similar to how it's done in 1.7.
+  
--- src/main/java/cc/woverflow/overflowanimations/MixcesHandler.java
+++ src/main/java/cc/woverflow/overflowanimations/MixcesHandler.java
@@ -17,75 +17,97 @@ public class MixcesHandler {
                         if (block instanceof BlockCarpet || block instanceof BlockSnow) {
                             applyThatWeirdTransformation();
                         } else if (block instanceof BlockFence) {
-                            GlStateManager.translate(0.0125, 0.1, -0.171875);
-                            GlStateManager.rotate(45, 0, 1, 0);
-                            GlStateManager.rotate(14, 1, 0, 0);
-                            GlStateManager.rotate(193, 0, 0, 1);
-                            GlStateManager.scale(0.375, 0.375, 0.375);
+                            GlStateManager.translate(0, -0.01, 0.04);
+                            GlStateManager.rotate(135, 0, 1, 0);
+                            GlStateManager.rotate(15, 1, 0, 0);
+                            GlStateManager.rotate(-15, 0, 0, 1);
+                            GlStateManager.scale(1, 1, 1);
                         } else if (block instanceof BlockFenceGate) {
-                            GlStateManager.translate(0.0125, 0.1, -0.171875);
-                            GlStateManager.rotate(-45, 0, 1, 0);
-                            GlStateManager.rotate(14, 1, 0, 0);
-                            GlStateManager.rotate(167, 0, 0, 1);
-                            GlStateManager.scale(0.375, 0.375, 0.375);
+                            GlStateManager.translate(-0.04, -0.01, 0);
+                            GlStateManager.rotate(135, 0, 1, 0);
+                            GlStateManager.rotate(10, 1, 0, 0);
+                            GlStateManager.rotate(7.5f, 0, 0, 1);
+                            GlStateManager.scale(1, 1, 1);
+                        } else if (block instanceof BlockChest || block instanceof BlockEnderChest) {
+                            GlStateManager.translate(0, 0, 0);
+                            GlStateManager.rotate(-90, 0, 1, 0);
+                            GlStateManager.rotate(0, 1, 0, 0);
+                            GlStateManager.rotate(0, 0, 0, 1);
+                            GlStateManager.scale(1, 1, 1);
+                        } else if (block instanceof BlockLadder || block instanceof BlockPane || block instanceof BlockRail || block instanceof BlockRailPowered || block instanceof BlockRailDetector || block instanceof BlockVine || block instanceof BlockWeb || block instanceof BlockLever || block instanceof BlockBush || block instanceof BlockTorch || block instanceof BlockTripWireHook) {
+                            GlStateManager.translate(0.0985, -0.02, -0.034);
+                            GlStateManager.rotate(-13, 0, 1, 0);
+                            GlStateManager.rotate(163, 1, 0, 0);
+                            GlStateManager.rotate(193, 0, 0, 1);
+                            GlStateManager.scale(1.0, 1.0, 1.0);
+                        } else if (block instanceof BlockFurnace || block instanceof BlockDispenser || block instanceof BlockPumpkin) {
+                            GlStateManager.translate(0.025, -0.01, -0.025);
+                            GlStateManager.rotate(180, 0, 1, 0);
+                            GlStateManager.rotate(5, 1, 0, 0);
+                            GlStateManager.rotate(5, 0, 0, 1);
+                            GlStateManager.scale(1, 1, 1);
+                        } else if (block instanceof BlockBanner) {
+                            GlStateManager.translate(0.01, -0.066, -0.007);
+                            GlStateManager.rotate(-4, 0, 1, 0);
+                            GlStateManager.rotate(-4, 1, 0, 0);
+                            GlStateManager.rotate(-20, 0, 0, 1);
+                            GlStateManager.scale(1.1, 1.1, 1.1);
+                        } else if (block instanceof BlockButton) {
+                            GlStateManager.translate(0.095, -0.04, -0.095);
+                            GlStateManager.rotate(0, 0, 1, 0);
+                            GlStateManager.rotate(-5, 1, 0, 0);
+                            GlStateManager.rotate(-5, 0, 0, 1);
+                            GlStateManager.scale(1, 1, 1);
+                        } else if (block != null) {
+                            GlStateManager.translate(-0.025, -0.01, 0.025);
+                            GlStateManager.rotate(0, 0, 1, 0);
+                            GlStateManager.rotate(-5, 1, 0, 0);
+                            GlStateManager.rotate(-5, 0, 0, 1);
+                            GlStateManager.scale(1, 1, 1);
                         } else {
                             OverflowAnimations.renderingStack = null;
                             return false;
                         }
                     } else {
                         if (stack.getItem() instanceof ItemBow) {
-                            GlStateManager.translate(0.0625, 0.0, -0.03125);
-                            GlStateManager.rotate(67, 0, 1, 0);
-                            GlStateManager.rotate(-6, 1, 0, 0);
-                            GlStateManager.rotate(-45, 0, 0, 1);
+                            GlStateManager.translate(0.037, 0.020, 0.007);
+                            GlStateManager.rotate(-15, 0, 1, 0);
+                            GlStateManager.rotate(0, 1, 0, 0);
+                            GlStateManager.rotate(-10, 0, 0, 1);
                             GlStateManager.scale(1, 1, 1);
                         } else if (stack.getItem() instanceof ItemFishingRod || stack.getItem() instanceof ItemCarrotOnAStick) {
-                            GlStateManager.translate(0.005, 0.035, -0.200625);
-                            GlStateManager.rotate(91, 0, 1, 0);
-                            GlStateManager.rotate(175, 1, 0, 0);
-                            GlStateManager.rotate(-56, 0, 0, 1);
-                            GlStateManager.scale(0.95, 0.95, 0.95);
-                        } else if (stack.getItem() instanceof ItemTool || stack.getItem() instanceof ItemHoe || stack.getItem().getUnlocalizedName().equals("item.stick")) {
-                            GlStateManager.translate(-0.003125, 0.031875, -0.1875);
-                            GlStateManager.rotate(88, 0, 1, 0);
+                            GlStateManager.translate(0.0, -0.052, -0.007);
+                            GlStateManager.rotate(-4, 0, 1, 0);
                             GlStateManager.rotate(-4, 1, 0, 0);
-                            GlStateManager.rotate(-54, 0, 0, 1);
-                            GlStateManager.scale(0.9, 0.9, 0.9);
+                            GlStateManager.rotate(-20, 0, 0, 1);
+                            GlStateManager.scale(1.1, 1.1, 1.1);
+                        } else if (stack.getItem() instanceof ItemTool || stack.getItem() instanceof ItemSword || stack.getItem() instanceof ItemHoe || stack.getItem().getUnlocalizedName().equals("item.stick") || stack.getItem().getUnlocalizedName().equals("item.blazeRod")) {
+                            GlStateManager.translate(0.01, -0.066, -0.007);
+                            GlStateManager.rotate(-4, 0, 1, 0);
+                            GlStateManager.rotate(-4, 1, 0, 0);
+                            GlStateManager.rotate(-20, 0, 0, 1);
+                            GlStateManager.scale(1.1, 1.1, 1.1);
+                        } else if (stack.getItem() instanceof ItemSkull) {
+                            GlStateManager.translate(-0.025, -0.01, 0.025);
+                            GlStateManager.rotate(0, 0, 1, 0);
+                            GlStateManager.rotate(15, 1, 0, 0);
+                            GlStateManager.rotate(15, 0, 0, 1);
+                            GlStateManager.scale(1, 1, 1);
+                        } else if (stack.getItem() != null) {
+                            GlStateManager.translate(0.0985, -0.02, -0.034);
+                            GlStateManager.rotate(-13, 0, 1, 0);
+                            GlStateManager.rotate(163, 1, 0, 0);
+                            GlStateManager.rotate(193, 0, 0, 1);
+                            GlStateManager.scale(1.0, 1.0, 1.0);
                         } else {
                             //applyThatWeirdTransformation();
                             OverflowAnimations.renderingStack = null;
                             return false;
                         }
                     }
-                } else if (stack.getItemUseAction() == EnumAction.BLOCK) {
-                    GlStateManager.translate(-0.00625, 0.001875, 0.004375);
-                    GlStateManager.rotate(0, 0.0f, 1.0f, 0.0f);
-                    GlStateManager.rotate(0, 1.0f, 0.0f, 0.0f);
-                    GlStateManager.rotate(0, 0.0f, 0.0f, 1.0f);
-                    GlStateManager.scale(1.0, 1.0, 1.0);
-                } else if (!(stack.getItem() instanceof ItemBlock)) {
-                    GlStateManager.translate(0, 0, 0);
-                    GlStateManager.rotate(0, 0.0f, 1.0f, 0.0f);
-                    GlStateManager.rotate(0, 1.0f, 0.0f, 0.0f);
-                    GlStateManager.rotate(0, 0.0f, 0.0f, 1.0f);
-                    GlStateManager.scale(1.0, 1.0, 1.0);
                 } else {
-                    Block block = ((ItemBlock) stack.getItem()).getBlock();
-                    if (block instanceof BlockCarpet || block instanceof BlockSnow) {
-                        /*/
-                        GlStateManager.translate(0, 0, 0);
-                        GlStateManager.rotate(0, 0.0f, 1.0f, 0.0f);
-                        GlStateManager.rotate(0, 1.0f, 0.0f, 0.0f);
-                        GlStateManager.rotate(0, 0.0f, 0.0f, 1.0f);
-                        GlStateManager.scale(1.0, 1.0, 1.0);
-
-                         */
-                        OverflowAnimations.renderingStack = null;
-                        return false;
-                    } else {
-                        OverflowAnimations.renderingStack = null;
-                        return false;
-                    }
+                    OverflowAnimations.renderingStack = null;
+                    return false;
                 }
                 OverflowAnimations.renderingStack = null;
                 return true;
@@ -97,11 +119,13 @@ public class MixcesHandler {
         }
     }
 
+    // Carpet & Snow Layer transformations )))):
     private static void applyThatWeirdTransformation() {
-        GlStateManager.translate(0.0, 0.096875, -0.15);
-        GlStateManager.rotate(-47.5F, 0, 1, 0);
-        GlStateManager.rotate(15, 1, 0, 0);
-        GlStateManager.rotate(165, 0, 0, 1);
-        GlStateManager.scale(0.375, 0.375, 0.375);
+        GlStateManager.translate(0.01, -0.2, -0.01);
+        GlStateManager.rotate(180, 0, 1, 0);
+        GlStateManager.rotate(5, 1, 0, 0);
+        GlStateManager.rotate(5, 0, 0, 1);
+        GlStateManager.scale(1, 1, 1);
     }
 }
+
