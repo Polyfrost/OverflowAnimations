3cf82572e3fbea6dc6621fc91fe056d1357e6b01
Mixces <70869960+Mixces@users.noreply.github.com>
2022-06-22 09:00:04 -0500

    awooga

--- src/main/java/cc/woverflow/overflowanimations/mixin/RendererLivingEntityMixin.java
+++ src/main/java/cc/woverflow/overflowanimations/mixin/RendererLivingEntityMixin.java
@@ -2,9 +2,10 @@ package cc.woverflow.overflowanimations.mixin;
 
 import cc.woverflow.overflowanimations.config.AnimationsConfig;
 import club.sk1er.oldanimations.AnimationHandler;
-import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RendererLivingEntity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,7 +17,14 @@ public abstract class RendererLivingEntityMixin<T extends EntityLivingBase> {
 
     @Redirect(method = "doRender(Lnet/minecraft/entity/EntityLivingBase;DDDFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/RendererLivingEntity;getSwingProgress(Lnet/minecraft/entity/EntityLivingBase;F)F"))
     private float redirectSwing(RendererLivingEntity<T> instance, T livingBase, float partialTickTime) {
-        if (AnimationsConfig.thirdPersonItemRendering && livingBase instanceof EntityPlayerSP) {
+        if(AnimationsConfig.thirdPersonSneaking && livingBase.isSneaking()) {
+                GlStateManager.translate(0,0.1,0);
+                if (!livingBase.isSneaking() && partialTickTime == 50) {
+                    GlStateManager.translate(0,1.1,0);
+                }
+        }
+
+        if (AnimationsConfig.thirdPersonItemRendering && livingBase instanceof EntityPlayer) {
             return AnimationHandler.getInstance().getSwingProgress(partialTickTime);
         } else {
             return getSwingProgress(livingBase, partialTickTime);
